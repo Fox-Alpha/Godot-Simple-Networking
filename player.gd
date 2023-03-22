ee250bb328e3ee4500c41494ac558af11479e9d9
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-03-22 16:28:51 +0100

    Fix: Error change Network Authority in _ready
    Fix: Error change Network Authority in _ready()
    ===: Debug Error with hint to _entered_tree()

--- player.gd
+++ player.gd
@@ -3,6 +3,17 @@ extends CharacterBody2D
 var team = "red"
 
 func _ready():
+	pass
+
+func _physics_process(_delta):
+	if not is_multiplayer_authority(): return
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 500
+	move_and_slide()
+
+func _spawn_custom() -> void:
+	pass
+
+func _on_tree_entered():
 	set_multiplayer_authority(str(name).to_int())
 
 	$Authority.visible = is_multiplayer_authority()
@@ -15,8 +26,3 @@ func _ready():
 
 	$PlayerName.text = get_tree().get_first_node_in_group("player_name").text
 	$PlayerName.modulate = Color(0, 1, 0)
-
-func _physics_process(_delta):
-	if not is_multiplayer_authority(): return
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 500
-	move_and_slide()
