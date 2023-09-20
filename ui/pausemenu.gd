466d890f35d292c2d746a3e721408a7544763497
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 20:29:18 +0200

    Add: Scene for Pause_Menu Add: Script for sendmessage

---
+++ ui/pausemenu.gd
@@ -0,0 +1,17 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+# ToDo: Wo ist der QuitButton ? Hier !
+func _on_quit_button_button_down():
+#	$Control/QuitConfirmation.show()
+	pass
