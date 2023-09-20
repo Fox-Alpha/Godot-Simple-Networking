a2d08c8d3811871f8fb6888240c49636f74c827c
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 20:27:56 +0200

    Mve: Moved to UI folder

--- ui/player_container.gd
+++ ui/player_container.gd
@@ -3,10 +3,10 @@ extends HBoxContainer
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
