7e45a1879c46f41edc5da35108cd7a6f8357f3a6
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 21:08:18 +0200

    Rem: Remove unrelated funcs Add: connect Signals

--- main.gd
+++ main.gd
@@ -1,36 +1,37 @@
 extends Node
 
-signal server_created
-
-var player = preload("res://player.tscn")
-var map = preload("res://map.tscn")
-
-var playerdata : Dictionary = { "Team_Blue": {}, "Team_Red": {} }
-
-var peer = ENetMultiplayerPeer.new()
-
 var enter_key_pressed = false
 
-@onready var playernode: Node = $Network/Player
 
-
-#func _process(_delta):
-#	if multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED:
-#		if %LobbyConnectedPlayers.visible:
-#			#Control/Lobby/MarginContainer/HBoxContainer/VBoxContainer2/ScrollContainer/LobbyConnectedPlayers
-#			display_players_connected(%LobbyConnectedPlayers)
+func _on_tree_exiting():
+	if multiplayer.has_multiplayer_peer():
+		var peers = multiplayer.get_peers()
+		if not multiplayer.is_server():
+			for p in peers:
+				multiplayer.disconnect_peer(p)
+				# .close_connection ( 100 )
+		else:
+			multiplayer.multiplayer_peer.close()
+		multiplayer.multiplayer_peer = null
+	pass # Replace with function body.
 
 
 func _ready():
-	$Control/Menu.show()
-	%SendMessage.position.y = get_viewport().size.y - (get_viewport().size.y / 6)
+	AL_Globals.playernode = get_node_or_null("Network/Player")
+	AL_Globals.mapnode = get_node_or_null("MapInstance")
+	AL_Globals.rootnode = get_tree().get_root()
+	%Menu.show()
 	%ChatBox.position.y = get_viewport().size.y - (get_viewport().size.y / 3) - 15
-	%SendMessage.hide()
 	%ChatBox.hide()
+	%SendMessage.position.y = get_viewport().size.y - (get_viewport().size.y / 6)
+	%SendMessage.hide()
 	%Scoreboard.hide()
-	$Control/Lobby.hide()
-	$Control/QuitConfirmation.hide()
-	server_created.connect(_on_server_created)
+	%Lobby.hide()
+	%QuitConfirmation.hide()
+
+
+#func _process(_delta):
+#	pass
 
 
 func _input(_event):
@@ -67,10 +68,6 @@ func _input(_event):
 		%ChatBoxDisapearsTimer.start()
 
 
-func _on_server_created():
-	add_player.rpc_id(1, multiplayer.multiplayer_peer.generate_unique_id(), "host")
-
-
 # Function to send a message
 # Client -> Server -> Alle Clients -> Client anzeige in UI
 @rpc("call_local", "any_peer")
@@ -101,8 +98,6 @@ func send_message(player_name, message, is_server):
 	%ChatBoxDisapearsTimer.start()
 
 
-
-
 ## Function to display players connected,
 ## it refreshes each time it is called on Clients
 #func display_players_connected(node : Node):
@@ -131,133 +126,20 @@ func send_message(player_name, message, is_server):
 #		lblplayer.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 #		HBox.add_child(lblplayer)
 
-# Networking system
-
-# Server
-
-
-
-
-
-func player_joined(id):
-	print("main::player_joined() [202] -> Player_Joined() : " + str(id))
-	pass
-
-
-# Client
-
-
-func server_connected():
-	pass
-
-
-@rpc("call_local", "any_peer")
-func add_player(id, team):
-	var player_instance = player.instantiate()
-	player_instance.name = str(id)
-	if multiplayer.is_server():
-		player_instance.get_node("ReferenceRect/PlayerName").text += " - HOST"
-#	player_instance.team = team
-
-	var pname = player_instance.get_node("ReferenceRect/PlayerName").text
-
-	match team:
-		"blue":
-			player_instance.team = Color.DODGER_BLUE
-			player_instance.global_position = %SpawnPosition/Blue.global_position
-
-			playernode.add_child(player_instance)
-#			player_instance.get_node("ReferenceRect/TeamColor").color = Color.DODGER_BLUE
-		"red":
-			player_instance.team = Color.ORANGE_RED
-			player_instance.global_position = %SpawnPosition/Red.global_position
-
-			playernode.add_child(player_instance)
-#			%SpawnPosition/Red.add_child(player_instance)
-#			player_instance.get_node("ReferenceRect/TeamColor").color = Color.ORANGE_RED
-		"host":
-
-			player_instance.team = Color.SEA_GREEN
-			player_instance.global_position = %SpawnPosition/Host.global_position
-
-			playernode.add_child(player_instance)
-#			%SpawnPosition/Host.add_child(player_instance)
-
-	if multiplayer.has_multiplayer_peer() and multiplayer.get_peers().has(id):
-		_on_server_display_players_connected(team)
-		# "User {} is {}.".format([42, "Godot"], "{}")
-		send_message.rpc(str(id), " ({} / {}) has joined the game".format([pname, team.capitalize()], "{}"), false)
-
-
-@rpc("authority")
-func load_game():
-#	%Menu.hide()
-#	%MapInstance.add_child(map_instance)
-
-#	$Control/Lobby.visible = !multiplayer.is_server()
-
-#	if multiplayer.is_server():
-#		display_players_connected(%PlayersConnectedListTeamBlue)
-#		%Scoreboard.show()
-	var map_instance = map.instantiate()
-
-func remove_player(id):
-
-	var pnode = %PlayersConnectedListTeamBlue.get_node_or_null(str(id))
-	if pnode != null:
-		pnode.queue_free()
-		var _player = playernode.get_node_or_null(str(id))
-		var pname = _player.get_node("ReferenceRect/PlayerName").text
-		_player.queue_free()
-		send_message.rpc(str(id), " ({} / {}) has leave the game".format([pname, "Blue".capitalize()], "{}"), false)
-	else:
-		pnode = %PlayersConnectedListTeamRed.get_node_or_null(str(id))
-		if (pnode != null):
-			pnode.queue_free()
-			var _player = playernode.get_node_or_null(str(id))
-			var pname = _player.get_node("ReferenceRect/PlayerName").text
-			send_message.rpc(str(id), " ({} / {}) has leave the game".format([pname, "Red".capitalize()], "{}"), false)
-			_player.queue_free()
-
-
-
-	# send_message.rpc(str(id), " left the game", false)
-
-func server_offline():
-	# Per Signal
-	quit_game()
-
-
-# ToDo: Wo ist der QuitButton ?
-func _on_quit_button_button_down():
-	$Control/QuitConfirmation.show()
-
-
-
 
 # ToDo: Per Signal aufrufen
 func quit_game():
+	# Close Connection(s)
 	%Lobby.hide()
 	%Menu.show()
 	%ChatBox.hide()
 	%SendMessage.hide()
 	%TypedMessage.text = ""
+	# Only on local player after disconnect, not on Server
 	%MapInstance.get_child(0).queue_free()
 
 
 
-
-func _on_tree_exiting():
-	if multiplayer.has_multiplayer_peer():
-		var peers = multiplayer.get_peers()
-		if not multiplayer.is_server():
-			for p in peers:
-				peer.disconnect_peer(p)
-				# .close_connection ( 100 )
-		else:
-			peer.close()
-		peer = null
-	pass # Replace with function body.
 
 
 
