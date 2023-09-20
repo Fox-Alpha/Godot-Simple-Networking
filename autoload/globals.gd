d231d7f55582b781debd3369360eb91022edeb66
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 20:32:05 +0200

    Add: Autoloads for Global Vars and Signals

---
+++ autoload/globals.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var player = preload("res://player.tscn")
+var map = preload("res://map.tscn")
+var player_container = preload("res://ui/player_container.tscn")
+
+var playerdata : Dictionary = { "Team_Blue": {}, "Team_Red": {} }
+
+var playernode: Node
+var mapnode: Node
+var rootnode: Node
---
+++ autoload/signalbus.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+signal host_server_created
+
+signal _do_server_create
+
+#signal host_server_created
