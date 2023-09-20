314002d71cd65cf4882c4f0c77fccf3470eae720
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 20:59:59 +0200

    Chg: Comment unused funcs chg: Func call create server with signal

--- ui/menu.gd
+++ ui/menu.gd
@@ -2,45 +2,22 @@ extends PanelContainer
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+#	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	pass
 
 func _on_username_text_submitted(_new_text):
 	_on_join_button_pressed()
 
 
 func _on_host_button_pressed():
-	if peer.create_server(9999) != OK:
-		print("Create Server failed !")
-		return
-
-	multiplayer.multiplayer_peer = peer
-
-	multiplayer.peer_disconnected.connect(remove_player)
-	multiplayer.peer_connected.connect(player_joined)
-
-	load_game()
-
-	server_created.emit()
+#	load_game()
+	AL_Signalbus._do_server_create.emit()
 
 
 func _on_join_button_pressed():
-	peer.create_client("localhost", 9999)
-	multiplayer.multiplayer_peer = peer
-
-	if %Username.text == "":
-		%Username.text = "Player"
-
-	if multiplayer.server_disconnected.is_connected(server_offline):
-		multiplayer.server_disconnected.disconnect(server_offline)
-		multiplayer.server_disconnected.connect(server_offline)
-	if multiplayer.connected_to_server.is_connected(server_connected):
-		multiplayer.connected_to_server.disconnect(server_connected)
-		multiplayer.connected_to_server.connect(server_connected)
-
-	load_game()
+	pass
