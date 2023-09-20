571371707570ae12fd614458648b4283b4e9f1ac
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-20 22:43:13 +0200

    Add: Signals for Client

--- autoload/signalbus.gd
+++ autoload/signalbus.gd
@@ -1,8 +1,7 @@
 extends Node
 
-
+signal _do_server_create
 signal host_server_created
 
-signal _do_server_create
-
-#signal host_server_created
+signal _do_client_create
+signal local_client_created
