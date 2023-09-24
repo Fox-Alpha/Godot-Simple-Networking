160316ce0f2dfb635ed65a8dc7ec9584b5fc955b
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-09-24 21:50:56 +0200

    Mve: display_players_connected to scoreboard UI script

--- ui/scoreboard.gd
+++ ui/scoreboard.gd
@@ -11,6 +11,52 @@ extends PanelContainer
 #	pass
 
 
+# Function to display players connected,
+# it refreshes each time it is called on Clients
+func display_players_connected(node : Node, team : String):
+	# Clear the previous list
+	if multiplayer.is_server():
+		return
+
+	if not is_instance_valid(node):
+		return
+
+	for c in node.get_children():
+		c.queue_free()
+
+	# Create the list of connected players
+	# TODO	: peerliste aus MultioplayerAPI verwenden
+	#		: Seperate Team Red and Team Blue
+	var _peer_node_list : Array[Node] = AL_Globals.playernode.get_children()
+#	print("perrlist: {0}, {1}".format([_peer_node_list[0].name, _peer_node_list[1].name]))
+#	_plist.append_array(playernode.get_children())
+	var _plist = multiplayer.get_peers()
+
+	for _p in _peer_node_list:
+		var _peer : Node = AL_Globals.playernode.get_node_or_null("{0}".format([_p]))
+
+		if _peer == null:
+#			print("peer node konnte nicht ermittelt werden")
+			break
+
+#		print("Getting peers count: {0}".format([_plist.size()]))
+
+		if not _peer.teamname == team:
+			print("Getting peer name, color, team: {0} / {1} / {2}".format([_peer.name, _peer.teamname, team]))
+			continue
+
+		var HBox := HBoxContainer.new()
+		HBox.name = str(_peer.name)
+		HBox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		node.add_child(HBox)
+#		print("Adding peer to list: {0}".format([_peer.name]))
+
+		var lblplayer = Label.new()
+		lblplayer.text = str(_peer.name)
+		lblplayer.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		HBox.add_child(lblplayer)
+
+
 func _on_player_kick_pressed(butt):
 #	var metavar : PackedStringArray = butt.get_meta_list()
 
